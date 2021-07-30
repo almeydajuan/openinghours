8168c22cd3310be4f7f2c7a35cdce6d96dc8dd2a
juan.almeyda <juan.almeyda@olx.com>
2021-07-30 14:36:16 +0200

    add parsing

--- src/test/kotlin/com/almeydajuan/openinghours/TestX.kt
+++ src/test/kotlin/com/almeydajuan/openinghours/TestX.kt
@@ -2,16 +2,37 @@ package com.almeydajuan.openinghours
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.Date
 
 class TestX {
 
     @Test
-    fun `convert unix time to human readable format`() {
-        assertEquals("9 AM", convert(32400))
+    fun `convert 10 AM unix time to human readable format`() {
+        assertEquals("10 AM", convert(32400))
+    }
+
+    @Test
+    fun `convert 11 30 AM unix time to human readable format`() {
+        assertEquals("11.30 AM", convert(37800))
     }
 
     private fun convert(unixTimestamp: Long): String {
-        return "9 AM"
+        val date = Date.from(Instant.ofEpochSecond(unixTimestamp))
+        val hourFormatter = SimpleDateFormat("HH")
+        val minutesFormatter = SimpleDateFormat("mm")
+        val hour = hourFormatter.format(date).toInt()
+        val minutes = resolveMinutes(minutesFormatter, date)
+        return "$hour$minutes AM"
     }
 
+    private fun resolveMinutes(minutesFormatter: SimpleDateFormat, date: Date): String {
+        val minutes = minutesFormatter.format(date).toString().toInt()
+        return if (minutes > 0) {
+            ".$minutes"
+        } else {
+            ""
+        }
+    }
 }
