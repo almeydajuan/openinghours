e77c96c9a7092dbeceb8b4b15359d54a319b1ace
juan.almeyda <juan.almeyda@olx.com>
2021-08-03 19:27:08 +0200

    Extract controller

--- src/main/kotlin/com/almeydajuan/openinghours/Main.kt
+++ src/main/kotlin/com/almeydajuan/openinghours/Main.kt
@@ -3,38 +3,22 @@ package com.almeydajuan.openinghours
 import com.almeydajuan.openinghours.parser.DayParser
 import com.almeydajuan.openinghours.parser.UnixTimestampParser
 import com.almeydajuan.openinghours.parser.WeekParser
-import com.almeydajuan.openinghours.provider.Day
 import com.almeydajuan.openinghours.service.ParsingService
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.fasterxml.jackson.module.kotlin.readValue
-import io.netty.buffer.ByteBufInputStream
 import io.vertx.core.Vertx
 import io.vertx.core.VertxOptions
-import io.vertx.core.buffer.Buffer
 import io.vertx.ext.web.Router
-import io.vertx.ext.web.handler.BodyHandler
 
 fun main() {
     val vertx = Vertx.vertx(VertxOptions())
-    val router = Router.router(vertx)
 
     val parsingService = ParsingService(WeekParser(DayParser(UnixTimestampParser())))
     val objectMapper = ObjectMapper().registerModule(KotlinModule())
+    val schedulesController = SchedulesController(objectMapper, parsingService)
 
-    router.route().handler(BodyHandler.create(false))
-    router.post("/api/schedules").handler { context ->
-        val body = context.body
-        val bodyAsJson = body.toJsonObject()
-        val workingWeek = Day.values().map { day ->
-            bodyAsJson.getJsonArray(day.input)?.let { jsonArray ->
-                val transitions: List<TransitionDto> = objectMapper.readValue(jsonArray.toBuffer())
-                WorkingDay(day.input, transitions.map { Transition(it.type, it.value.toLong()) })
-            } ?: WorkingDay(day.input, emptyList())
-        }
-        val parsedOpeningHours = parsingService.parseOpeningHours(workingWeek)
-        context.response().setStatusCode(200).end(parsedOpeningHours)
-    }
+
+    val router = Router.router(vertx).also { schedulesController.installRoute(it) }
 
     vertx.createHttpServer()
         .requestHandler(router)
@@ -43,7 +27,3 @@ fun main() {
             println("HTTP server started on port " + server.actualPort())
         }
 }
-
-inline fun <reified T> ObjectMapper.readValue(src: Buffer): T = readValue(ByteBufInputStream(src.byteBuf))
-
-private data class TransitionDto(val type: String, val value: String)
---
+++ src/main/kotlin/com/almeydajuan/openinghours/SchedulesController.kt
@@ -0,0 +1,35 @@
+package com.almeydajuan.openinghours
+
+import com.almeydajuan.openinghours.provider.Day
+import com.almeydajuan.openinghours.service.ParsingService
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.netty.buffer.ByteBufInputStream
+import io.vertx.core.buffer.Buffer
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.BodyHandler
+
+class SchedulesController(
+    private val objectMapper: ObjectMapper,
+    private val parsingService: ParsingService
+) {
+    fun installRoute(router: Router) {
+        router.route().handler(BodyHandler.create(false))
+        router.post("/api/schedules").handler { ctx ->
+            val body = ctx.body
+            val bodyAsJson = body.toJsonObject()
+            val workingWeek = Day.values().map { day ->
+                bodyAsJson.getJsonArray(day.input)?.let { jsonArray ->
+                    val transitions: List<TransitionDto> = objectMapper.readValue(jsonArray.toBuffer())
+                    WorkingDay(day.input, transitions.map { Transition(it.type, it.value.toLong()) })
+                } ?: WorkingDay(day.input, emptyList())
+            }
+            val parsedOpeningHours = parsingService.parseOpeningHours(workingWeek)
+            ctx.response().setStatusCode(200).end(parsedOpeningHours)
+        }
+    }
+}
+
+inline fun <reified T> ObjectMapper.readValue(src: Buffer): T = readValue(ByteBufInputStream(src.byteBuf))
+
+private data class TransitionDto(val type: String, val value: String)
